f3bd3f1f07f93837d11bd15d1be8873f2cc688f6
jingx <xin.jing1@thoughtworks.com>
2020-06-13 23:31:19 +0800

    [xin] minor bug fix

--- src/practice2.js
+++ src/practice2.js
@@ -2,7 +2,7 @@ const inject = (items,sections) =>{
 
     sections.sort(compareByIndex("index"));
     for (let counter in sections){
-        let index = sections[counter].index + parseInt(counter);
+        let index = sections[counter].index;
         let content = sections[counter].content
         items.splice(index, 0, content);
     }
@@ -13,7 +13,7 @@ function compareByIndex(p){
     return function(m,n){
         let a = m[p];
         let b = n[p];
-        return a - b;
+        return b - a;
     }
 }
 
