91c9b68917b37738b06f65bd2ca55e6c5e159ca2
jingx <xin.jing1@thoughtworks.com>
2020-06-13 23:24:21 +0800

    [xin] finished

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = input => {
+    let result = [];
+    for(let content of input.data){
+        let temp ={};
+        for(let counter in input.column){
+            let key = input.column[counter].name;
+            temp[key] = content[counter];
+        }
+        result.push(temp);
+    }
+    return result;
+}
 export { parseData };
+
+
+
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
+const inject = (items,sections) =>{
+
+    sections.sort(compareByIndex("index"));
+    for (let counter in sections){
+        let index = sections[counter].index + parseInt(counter);
+        let content = sections[counter].content
+        items.splice(index, 0, content);
+    }
+    return items;
+}
+
+function compareByIndex(p){
+    return function(m,n){
+        let a = m[p];
+        let b = n[p];
+        return a - b;
+    }
+}
+
 export { inject };
